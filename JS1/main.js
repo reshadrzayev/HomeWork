c49ee297f632821fb397f818ccd1b0c21a547e72
Rəşad Rzayev <reshadrzayev00@gmail.com>
2021-03-30 01:39:36 +0400

    First JavaScript

---
+++ JS1/main.js
@@ -0,0 +1,20 @@
+function myFunc1(){
+    document.getElementById('aze').innerHTML='Baku'
+    document.getElementById('azeabout').innerHTML
+    document.getElementById('azeabout').style.display="block";
+    document.getElementById('bakuimg').style.display="block"
+}
+function myFunc2(){
+    document.getElementById('rus').innerHTML='Moscow'
+    document.getElementById('rusabout').innerHTML
+    document.getElementById('rusabout').style.display="block";
+    document.getElementById('moscowimg').style.display="block"
+
+}
+function myFunc3(){
+    document.getElementById('turk').innerHTML='Ankara'
+    document.getElementById('turkabout').innerHTML
+    document.getElementById('turkabout').style.display="block";
+    document.getElementById('ankimg').style.display="block"
+
+}
