0bca3a546f95c58ef4a4ff3f8bd209ff4aa719ae
Rəşad Rzayev <reshadrzayev00@gmail.com>
2021-04-01 12:01:09 +0400

    JavaScript 2

--- JS2/main.js
+++ JS2/main.js
@@ -9,6 +9,8 @@
 var sehervaxti=6;
 var gunortavaxti=13;
 var axsamvaxti=18;
+var gecevaxti=24;
+
 
 function myFunc(){
     var saat = document.getElementById("saat").value;
@@ -19,7 +21,7 @@ function myFunc(){
         document.getElementById('txtseher').style.display="block";
         document.getElementById('txtgunorta').style.display="none";
         document.getElementById('txtaxsham').style.display="none";
-
+        document.getElementById('txtgece').style.display="none";
 
         document.getElementById("div").style.backgroundImage="url('morning.jfif')";
     }
@@ -28,24 +30,35 @@ function myFunc(){
         document.getElementById('txtgunorta').style.display="block";
         document.getElementById('txtseher').style.display="none";
         document.getElementById('txtaxsham').style.display="none";
-
-
+        document.getElementById('txtgece').style.display="none";
         
         document.getElementById("div").style.backgroundImage="url('afternoon.jfif')";
 
     }
-    else if(saat>=axsamvaxti){
+    else if(saat>=axsamvaxti && saat<=gecevaxti){
         // document.write(axsam);
         document.getElementById('txtaxsham').style.display="block";
         document.getElementById('txtseher').style.display="none";
         document.getElementById('txtgunorta').style.display="none";
+        document.getElementById('txtgece').style.display="none";
+
 
 
         document.getElementById("div").style.backgroundImage="url('evening.jpeg')";
 
     }
+    else if(saat>=0 && saat<6){
+        document.getElementById('txtgece').style.display="block";
+        document.getElementById('txtseher').style.display="none";
+        document.getElementById('txtgunorta').style.display="none";
+        document.getElementById('txtgunorta').style.display="none";
+
+
+        document.getElementById("div").style.backgroundColor="black";
+
+    }
     else{
-        document.write="error";
+        alert("Error")
     }
 }
 
