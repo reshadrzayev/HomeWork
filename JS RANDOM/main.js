0e90e697b854a38c3dfbbd0932872e47a139215e
Rəşad Rzayev <reshadrzayev00@gmail.com>
2021-04-13 11:08:48 +0400

    Random

--- JS RANDOM/main.js
+++ JS RANDOM/main.js
@@ -1,47 +1,39 @@
 var trying=5;
-
-
 var num=Math.floor((Math.random()*10)+1);
-
-
-
-
+var times=0;
+ 
 function show(){
     var guess=document.getElementById("guessTxt").value;
-
+    
     while(trying>0)
     {
+        
         trying--;
-        if(num==guess){
-            alert("Təbriklər doğru təxmin. Qalan cəhd sayı:"+trying);
-            
-            // res="tebrikler";
+        times++;
+        
+        if(trying==0){
+            alert("Cehd bitti");
             break;
-        }
-        else if(num>guess){
-            // res="yuxari";
-            
-
-            alert("Daha Yuxarı. Qalan cəhd sayı:"+trying);
-            break;
-
         }
         else{
-
-            alert("Daha Aşağı. Qalan cəhd sayı:"+trying);
-            break;
-            // res="asagi";
+            if(num==guess){
+                alert("Təbriklər doğru təxmin. "+ times + "ci dəfədə bildiniz!");
+                document.getElementById("xal").innerHTML=100-(20*(times-1));
+                break;
+            }
+            else if(num>guess){
+                alert("Daha Yuxarı. Qalan cəhd sayı: "+trying);
+                break;
+            }
+            else{
+                alert("Daha Aşağı. Qalan cəhd sayı: "+trying);
+                break;
+            }
         }
 
-
-        // if(trying==0){
-        //     alert("Cehd bitti");
-        //     break;
-        // //    res="cehdiniz bitti";
-        // }
+        
 
 
     }
-    // document.getElementById('result').innerHTML=res;
 
 }
