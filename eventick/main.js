84628b45abe4879c8676ebb8f4eb0ea9e8de9ce1
Rəşad Rzayev <reshadrzayev00@gmail.com>
2021-05-03 17:35:12 +0400

    Eventick Template

--- eventick/main.js
+++ eventick/main.js
@@ -5,24 +5,18 @@ $(function(){
         $(".spkr3").css("display","none");
         $(".spkr4").css("display","none");
     });
-});
-$(function(){
     $("#btn2").click(function(){
         $(".spkr2").css("display","flex");
         $(".spkr1").css("display","none");
         $(".spkr3").css("display","none");
         $(".spkr4").css("display","none");
     });
-});
-$(function(){
     $("#btn3").click(function(){
         $(".spkr3").css("display","flex");
         $(".spkr1").css("display","none");
         $(".spkr2").css("display","none");
         $(".spkr4").css("display","none");
     });
-});
-$(function(){
     $("#btn4").click(function(){
         $(".spkr4").css("display","flex");
         $(".spkr1").css("display","none");
@@ -30,3 +24,46 @@ $(function(){
         $(".spkr3").css("display","none");
     });
 });
+
+
+
+$(function(){
+    $("#button1").click(function(){
+        $(".row1-content").toggle(200);
+        $(".row2-content").hide(200);
+        $(".row3-content").hide(200);
+    });
+    $(".roww2").click(function(){
+        $(".row2-content").toggle(200);
+        $(".row1-content").hide(200);
+        $(".row3-content").hide(200);
+    });
+    $(".roww3").click(function(){
+        $(".row3-content").toggle(200);
+        $(".row1-content").hide(200);
+        $(".row2-content").hide(200);
+    });
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,  
+    autoplay:true,
+    dots: false,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    },
+})
+
+
+
